4812daffa61285eb6bd813a03a6c30ada014a233
Valtrizt Khalifah Warri <valvaltrizt@gmail.com>
2024-07-27 19:50:01 +0700

    feat(actions): Added Group Transaction By Month-Year

--- lib/data/getTransactions.ts
+++ lib/data/getTransactions.ts
@@ -16,3 +16,36 @@ export async function getTransactions() {
 
   return data;
 }
+
+export async function getTransactionsByMonth() {
+  const supabase = createClient();
+
+  const { data, error } = await supabase
+    .from('transactions')
+    .select('*');
+
+  if (error) {
+    console.log(error);
+    return error;
+  }
+
+  const categorizedTransactions: Record<string, Record<string, any[]>> = {};
+
+  data.forEach((transaction: any) => {
+    const date = new Date(transaction.date);
+    const year = date.getFullYear();
+    const month = date.toLocaleString('default', { month: 'long' });
+
+    if (!categorizedTransactions[year]) {
+      categorizedTransactions[year] = {};
+    }
+
+    if (!categorizedTransactions[year][month]) {
+      categorizedTransactions[year][month] = [];
+    }
+
+    categorizedTransactions[year][month].push(transaction);
+  });
+
+  return categorizedTransactions;
+}
