ca08d201f7da2b964ebdc339ca6f9eae2fc48d8a
Ramadhika123 <ramadhikadarma0607@gmail.com>
2024-07-27 22:39:53 +0700

    fix: Handling many chat from user

--- app/dashboard/[userId]/ai-chat/page.tsx
+++ app/dashboard/[userId]/ai-chat/page.tsx
@@ -50,7 +50,7 @@ export default function Page() {
     const formattedContent = marked(message.content);
     const isUser = message.role === 'user';
     return (
-      <div key={index} className={`flex items-start p-2 rounded-lg ${isUser ? 'bg-white' : 'bg-emerald-500'}`}>
+      <div key={index} className={`flex items-start p-4 rounded-lg ${isUser ? 'bg-white mb-5' : 'bg-emerald-500 mb-5'}`}>
         {isUser ? <UserIcon className="mr-2 text-black" /> : <Bot className="mr-2 text-emerald-950" />}
         <div dangerouslySetInnerHTML={{ __html: formattedContent }} />
       </div>
@@ -64,46 +64,44 @@ export default function Page() {
   ];
 
   return (
-    <div className="flex flex-col justify-center h-full">
-      <div className="w-full rounded-lg h-5/6">
-        <div className="mb-4 space-y-2 overflow-auto h-full">
-          {conversation.map((message, index) => renderMessage(message, index))}
-          {!hasConversationStarted && (
-            <div>
-              <h1 className="text-6xl text-white mb-10">
-                Hello! <br />
-                Need assistance with your business <br /> analysis? I'm here to help!
-              </h1>
-              <div className="flex flex-row space-x-4 container justify-center items-top ">
-                {predefinedMessages.map((message, index) => (
-                  <button key={index} onClick={() => handleSendMessage(message)} className="p-5 w-5/6 h-52 max-h-15 font-bold text-neutral-700 bg-white rounded-lg hover:bg-slate-200 text-start">
-                    <MessageCircleQuestion className="text-yellow-500" />
-                    <p className="mt-4">{message}</p>
-                  </button>
-                ))}
-              </div>
+    <>
+      <div className="w-full flex-grow overflow-auto p-4" style={{ maxHeight: 'calc(100% - 64px)' }}>
+        {conversation.map((message, index) => renderMessage(message, index))}
+        {!hasConversationStarted && (
+          <div>
+            <h1 className="text-6xl text-white mb-10">
+              Hello! <br />
+              Need assistance with your business <br /> analysis? I'm here to help!
+            </h1>
+            <div className="flex flex-row justify-between w-full gap-2">
+              {predefinedMessages.map((message, index) => (
+                <button key={index} onClick={() => handleSendMessage(message)} className="p-3 w-5/6 h-40 max-h-15 font-bold text-neutral-700 bg-white rounded-lg hover:bg-slate-200 text-start ">
+                  <MessageCircleQuestion className="text-yellow-500" />
+                  <p className="mt-2">{message}</p>
+                </button>
+              ))}
             </div>
-          )}
-        </div>
-
-        <div className="relative flex items-center">
-          <input
-            type="text"
-            value={input}
-            onChange={(event) => setInput(event.target.value)}
-            className="flex-grow p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-emerald-200"
-            placeholder="Type your message..."
-            onKeyDown={(event) => {
-              if (event.key === 'Enter') {
-                handleSendMessage(input);
-              }
-            }}
-          />
-          <button onClick={() => handleSendMessage(input)} className="absolute right-2 px-4 py-2 font-bold text-white">
-            <SendHorizontalIcon className="text-black" />
-          </button>
-        </div>
+          </div>
+        )}
       </div>
-    </div>
+
+      <div className="flex items-center p-4 sticky bottom-0 ">
+        <input
+          type="text"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+          className="flex-grow p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-emerald-200"
+          placeholder="Type your message..."
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              handleSendMessage(input);
+            }
+          }}
+        />
+        <button onClick={() => handleSendMessage(input)} className="absolute right-2 px-4 py-2 font-bold text-white">
+          <SendHorizontalIcon className="text-black" />
+        </button>
+      </div>
+    </>
   );
 }
