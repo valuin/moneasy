4b7dbb4af9192cf583099f941866a1ee22fdf32d
Ramadhika123 <ramadhikadarma0607@gmail.com>
2024-07-27 18:07:14 +0700

    feat: New styling for AI chat

--- app/dashboard/[userId]/ai-chat/page.tsx
+++ app/dashboard/[userId]/ai-chat/page.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { Message, continueConversation } from '@/lib/actions/getAI';
 import { readStreamableValue } from 'ai/rsc';
 import { marked } from 'marked';
+import { Bot, SendHorizontalIcon, UserIcon } from 'lucide-react';
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -11,6 +12,9 @@ export const maxDuration = 30;
 export default function Page() {
   const [conversation, setConversation] = useState<Message[]>([]);
   const [input, setInput] = useState<string>('');
+  const [hasConversationStarted, setHasConversationStarted] = useState<boolean>(false);
+  const [generation, setGeneration] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSendMessage = async () => {
     // Append the user's message to the conversation
@@ -19,6 +23,7 @@ export default function Page() {
 
     // Clear the input field
     setInput('');
+    setHasConversationStarted(true);
 
     // Send the message to the server and get the streamable response
     const { messages, newMessage } = await continueConversation(newConversation);
@@ -46,23 +51,48 @@ export default function Page() {
 
   const renderMessage = (message: Message, index: number) => {
     const formattedContent = marked(message.content);
-    return <div key={index} className={`p-2 rounded-lg ${message.role === 'user' ? 'bg-blue-100' : 'bg-emerald-500'}`} dangerouslySetInnerHTML={{ __html: formattedContent }} />;
+    const isUser = message.role === 'user';
+    return (
+      <div key={index} className={`flex items-start p-2 rounded-lg ${isUser ? 'bg-white' : 'bg-emerald-500'}`}>
+        {isUser ? <UserIcon className="mr-2 text-black" /> : <Bot className="mr-2 text-emerald-950" />}
+        <div dangerouslySetInnerHTML={{ __html: formattedContent }} />
+      </div>
+    );
   };
 
   return (
-    <div className="flex flex-row justify-center h-full">
-      <div className="w-full p-4 bg-white rounded-lg shadow-md">
-        <div className="mb-4 space-y-2 overflow-auto h-5/6">{conversation.map((message, index) => renderMessage(message, index))}</div>
-        <div className="flex">
+    <div className="flex flex-col justify-center h-full">
+      <div className="w-full rounded-lg h-5/6">
+        <div className="mb-4 space-y-2 overflow-auto h-full">
+          {conversation.map((message, index) => renderMessage(message, index))}
+          {!hasConversationStarted && (
+            <div>
+              <h1 className="text-6xl text-white mb-4">
+                Hello! <br />
+                Need assistance with your business <br /> analysis? I'm here to help!
+              </h1>
+              <button onClick={() => setHasConversationStarted(true)} className="px-4 py-2 font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                Generate
+              </button>
+            </div>
+          )}
+        </div>
+
+        <div className="relative flex items-center">
           <input
             type="text"
             value={input}
             onChange={(event) => setInput(event.target.value)}
-            className="flex-grow p-2 mr-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="flex-grow p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-emerald-200"
             placeholder="Type your message..."
+            onKeyDown={(event) => {
+              if (event.key === 'Enter') {
+                handleSendMessage();
+              }
+            }}
           />
-          <button onClick={handleSendMessage} className="px-4 py-2 font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-            Send
+          <button onClick={handleSendMessage} className="absolute right-2 px-4 py-2 font-bold text-white">
+            <SendHorizontalIcon className="text-black" />
           </button>
         </div>
       </div>
