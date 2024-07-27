9200e4a644d6b3f4251ae188771244cb82a0146c
Ramadhika123 <ramadhikadarma0607@gmail.com>
2024-07-27 15:03:00 +0700

    punya dhika
    chat ai dan landing page progres 1

---
+++ components/landing-page/hero-section.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+export default function HeroSection() {
+  return (
+    <div className="flex flex-col items-center mt-20">
+      <h1 className="text-6xl text-white text-center">
+        Empower Your Decisions with <br />
+        Insightful Analytics
+      </h1>
+      <p className="text-white text-center mt-10">Explore New Possibilities with Advanced Data Insights</p>
+    </div>
+  );
+}
